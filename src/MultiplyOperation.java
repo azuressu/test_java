b5b7ed1bcafff04590b166911a3cbd065acf22da
azuressu <martina0130@naver.com>
2023-05-24 20:37:32 +0900

    update MultiplyOperation.java

--- src/MultiplyOperation.java
+++ src/MultiplyOperation.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class MultiplyOperation {
+public class MultiplyOperation {
+    int firstNumber, secondNumber;
+
+    public double operate(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        return (double) this.firstNumber * this.secondNumber;
+    }
 }
