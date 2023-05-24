978dd4f16df0e1c300d568756e36294f0fbefc18
azuressu <martina0130@naver.com>
2023-05-24 20:37:55 +0900

    update SubstractOperation

--- src/SubstractOperation.java
+++ src/SubstractOperation.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class SubstractOperation {
+public class SubstractOperation {
+
+    int firstNumber, secondNumber;
+
+    public double operate(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        return (double) this.firstNumber - this.secondNumber;
+    }
 }
