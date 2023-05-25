1788093ced78931d701c0da166416fce5cd84604
azuressu <martina0130@naver.com>
2023-05-25 11:32:47 +0900

    second update Calculator.java and new add Main.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,31 @@
 public class Calculator {
+    private final AddOperation addOperation;
+    private final SubstractOperation substractOperation;
+    private final MultiplyOperation multiplyOperation;
+    private final DivideOperation divideOperation;
 
-    String operator;
-    int firstNumber, secondNumber;
-
+    public Calculator(AddOperation addOperation, SubstractOperation substractOperation,
+                      MultiplyOperation multiplyOperation, DivideOperation divideOperation) {
+        this.addOperation = addOperation;
+        this.substractOperation = substractOperation;
+        this.multiplyOperation = multiplyOperation;
+        this.divideOperation = divideOperation;
+    }
     public double calculate (String operator, int firstNumber, int secondNumber) {
-        this.operator = operator;
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
         // return이 있어서 break를 안해주어도 되는 것인가 ..
-        switch (this.operator) {
+        switch (operator) {
             case "+": // 더하기
                 AddOperation addOperation = new AddOperation();
-                return addOperation.operate(this.firstNumber, this.secondNumber);
+                return addOperation.operate(firstNumber, secondNumber);
             case "-": // 빼기
                 SubstractOperation substractOperation = new SubstractOperation();
-                return substractOperation.operate(this.firstNumber, this.secondNumber);
+                return substractOperation.operate(firstNumber, secondNumber);
             case "/": // 나누기
-                DivideOpertaion divideOpertaion = new DivideOpertaion();
-                return divideOpertaion.operate(this.firstNumber, this.secondNumber);
+                DivideOperation divideOpertaion = new DivideOperation();
+                return divideOpertaion.operate(firstNumber, secondNumber);
             case "*": // 곱하기
                 MultiplyOperation multiplyOperation = new MultiplyOperation();
-                return multiplyOperation.operate(this.firstNumber, this.secondNumber);
+                return multiplyOperation.operate(firstNumber, secondNumber);
 //            case "%": // 나머지
 //                return (double) this.firstNumber % this.secondNumber;
             default: // default 값에 return 안해주면 오류남
