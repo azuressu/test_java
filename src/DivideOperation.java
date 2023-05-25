5d5cb303ed19b7a22ff0c1115fb5fee778ac379e
azuressu <martina0130@naver.com>
2023-05-25 11:28:18 +0900

    second update DivideOperation.java - step3

--- src/DivideOperation.java
+++ src/DivideOperation.java
@@ -1,10 +1,5 @@
 public class DivideOperation {
-
-    int firstNumber, secondNumber;
-
     public double operate(int firstNumber, int secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        return (double) this.firstNumber / this.secondNumber;
+        return (double) firstNumber / secondNumber;
     }
 }
