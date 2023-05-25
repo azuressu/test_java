1a1d3636db31bb0ccd2b8a939f3b7211a7b9e4ea
azuressu <martina0130@naver.com>
2023-05-25 11:27:24 +0900

    second update SubstractOperation.java - step3

--- src/SubstractOperation.java
+++ src/SubstractOperation.java
@@ -1,10 +1,6 @@
 public class SubstractOperation {
 
-    int firstNumber, secondNumber;
-
     public double operate(int firstNumber, int secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        return (double) this.firstNumber - this.secondNumber;
+        return (double) firstNumber - secondNumber;
     }
 }
